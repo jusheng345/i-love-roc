90fef61a1d81d83211a20c54c1b92858c52b0396
JU <chris34651@gmail.com>
2023-05-12 11:18:19 +0800

    model view url initialized

--- teachingplan/upload/models.py
+++ teachingplan/upload/models.py
@@ -1,3 +1,51 @@
+
 from django.db import models
 
-# Create your models here.
+class Grade(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    classes = models.ManyToManyField(Class, related_name='courses')
+
+    def __str__(self):
+        return f'{self.subject} - {self.name} ({self.teacher})'
+
+class Document(models.Model):
+    name = models.CharField(max_length=100)
+    file = models.FileField(upload_to='documents/')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# Grade：年級
+# Teacher：教師
+# Subject：科目
+# Class：班級
+# Course：課程，包含了教師、科目、班級等資訊
+# Document：文件，包含了上傳的課程計畫等資訊，與課程是一對多的關係
--- teachingplan/upload/views.py
+++ teachingplan/upload/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Grade, Teacher, Subject, Class, Course, Document
 
-# Create your views here.
+def index(request):
+    grades = Grade.objects.all()
+    teachers = Teacher.objects.all()
+    subjects = Subject.objects.all()
+    classes = Class.objects.all()
+    courses = Course.objects.all()
+    documents = Document.objects.all()
+    context = {
+        'grades': grades,
+        'teachers': teachers,
+        'subjects': subjects,
+        'classes': classes,
+        'courses': courses,
+        'documents': documents
+    }
+    return render(request, 'index.html', context)
+
+def course_detail(request, course_id):
+    course = Course.objects.get(pk=course_id)
+    documents = Document.objects.filter(course=course)
+    context = {
+        'course': course,
+        'documents': documents
+    }
+    return render(request, 'course_detail.html', context)
